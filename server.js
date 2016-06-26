43a94c948c6f63d28e806548bcd9e0cd9c3a5bd5
nogainbar <nogaenbar@MBPro-2.local>
2016-06-26 11:52:29 +0100

    send my response from server successfully!

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var http = require('http');
 
-var server = http.createServer();
+var message = 'I am so happy to be doing this workshop!';
+
+function handler (request, response) {
+  response.writeHead (200, {"Content-Type" : "text/html"});
+  response.write(message); //response body
+  response.end(); //finish response
+
+}
+
+var server = http.createServer(handler);
 
 server.listen(3000, function(){
   console.log("Server is listening on port 3000. Ready to accept requests!");

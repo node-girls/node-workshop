f949850fc74303f97f1324ec91488a1c1e034a3a
nogainbar <nogaenbar@MBPro-2.local>
2016-06-27 21:32:44 +0100

    i made it almost happened, endpoints work but not script and css files

--- server.js
+++ server.js
@@ -1,35 +1,47 @@
 var http = require('http');
 var fs = require('fs');
 
-var message = 'reading files';
+var message1 = 'endpoint is node!';
+var message2 = 'endpoint is girls!';
+var message3 = 'reading files';
+var files = [''];
+
+var port = 4000;
 
 function handler (request, response) {
+  var url = request.url;
 
-  if(endpoint === "/"){
-    response.writeHead (200, {"Content-Type" : "text/html"});
-
+  if(url.length === 1){
     fs.readFile(__dirname + '/public/index.html', function(error, file){
       if(error) {
         console.log(error);
-        return;
+        response.end();
+      } else {
+        response.writeHead (200, {'Content-Type': 'text/html'});
+        response.end(file);
       }
+    });
 
-      response.end(file);
+  } else if (url.indexOf('/node') > -1 ){
+    response.writeHead (200, {'Content-Type': 'text/html'});
+    response.end(message1); //response body
+  } else if (url.indexOf('/girls') > -1){
+    response.writeHead (200, {'Content-Type': 'text/html'});
+    response.end(message2); //response body
+  } else {
+    fs.readFile(__dirname + url, function(error, file){
+      if(error) {
+        console.log(error);
+        response.end();
+      } else {
+        var ext = url.split('.')[1];
+        response.writeHead (200, {'Content-Type': 'text/' + ext});
+        response.end(file);
+      }
     });
   }
-
 }
-//   var method = request.method;
-//   console.log(method);
-//   var endpoint = request.url;
-//   console.log(endpoint);
-//   response.write(message); //response body
-//   response.end(); //finish response
-//
-// }
-//
-// var server = http.createServer(handler);
-//
-// server.listen(3000, function(){
-//   console.log("Server is listening on port 3000. Ready to accept requests!");
-// });
+
+http.createServer(handler).listen(port);
+
+console.log('im listening! this is port: ' + port);

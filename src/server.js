d6d3713edd2d66a51a40ceaa6eb5b1a06b56af3d
matthewdking <matthewdking@outlook.com>
2017-07-23 15:23:12 +0300

    step 4

--- src/server.js
+++ src/server.js
@@ -1,8 +1,41 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 const handler = (req, res) =>{
+  var endpoint = req.url;
+  console.log(endpoint);
+  var method = req.method;
+  console.log(method);
+  if(endpoint === '/'){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    console.log(__dirname+'/public/index.html');
+    var fPath = path.join(__dirname, '..', '/public/index.html');
+    fs.readFile(fPath, (err, file)=>{
+      if(err){
+        console.log(err);
+        return;
+      }
+      res.end(file);
+    })
+  }
+  // if(endpoint === '/node'){
+  //   res.writeHead(200,{'Content-Type':'text/html'});
+  //   res.write('node');
+  //   res.end();
+  // }
+  // else if (endpoint === '/girls'){
+  //   res.writeHead(200,{'Content-Type':'text/html'});
+  //   res.write('girls');
+  //   res.end();
+  // }
+else{
   res.writeHead(200,{'Content-Type':'text/html'});
   res.write(message);
   res.end();
+
+}
+
 }
 var server = http.createServer(handler);
 

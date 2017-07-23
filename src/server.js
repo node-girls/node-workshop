44020eeb7ddb235382ff5082523e3dea9df6e313
matthewdking <matthewdking@outlook.com>
2017-07-23 15:00:53 +0300

    step3

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const http = require('http');
-
-var server = http.createServer();
+const handler = (req, res) =>{
+  res.writeHead(200,{'Content-Type':'text/html'});
+  res.write(message);
+  res.end();
+}
+var server = http.createServer(handler);
 
 var port = 3000;
+
+const message = 'hello world';
+
 
 server.listen(port, function(){
   console.log(`Server is listing on port: ${port}`)

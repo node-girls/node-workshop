bfc61808ad117f326b61da29bb62614c44418bfc
walidmash <waleedmash2@gmail.com>
2017-07-30 15:03:44 +0300

    make the server.js

---
+++ src/router.js
@@ -0,0 +1,5 @@
+function router() {
+  
+}
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var router = require('./router.js');
+var server = http.createServer();
+
+
+server.listen(4000 , function() {
+    console.log('the server is runnning on port : ' +4000);
+})

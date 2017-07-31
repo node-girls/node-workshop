22877e8a27a145e799eac6d9ce086e9d776475a5
walidmash <waleedmash2@gmail.com>
2017-07-31 15:16:15 +0300

    remove nodemon from package.json

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 var http = require('http');
 var router = require('./router.js');
 var PORT = 4000;
-
 var server = http.createServer(router);
-
 
 server.listen(process.env.PORT || PORT , () => {
   console.log(`Server running on ${PORT}`);

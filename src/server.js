f72e49a1c0a10c72badf7014fe3043c7ac20d87e
tawfiknasser <tawfiknasser@gmail.com>
2018-07-22 15:18:29 +0300

    changed handler function added endpoints with file

--- src/server.js
+++ src/server.js
@@ -1,12 +1,36 @@
-var http = require('http');
-var server=http.createServer(handler);
+var http = require("http");
+var fs = require("fs");
+var server = http.createServer(handler);
 
-server.listen(5000,function(){
+server.listen(5000, function() {
   //console.log("server is ready to go !!!!!!!! :)");
 });
 
-function handler(request,response){
-  response.writeHead(200,{"Content-Type": "text/html"});
-  response.write("hhh");
-  response.end();
-};
+function handler(request, response) {
+  var endPoint = request.url;
+  var method = request.method;
+  console.log(method);
+  console.log(endPoint);
+  if (endPoint === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile(__dirname+'..'+'/public/index.html',function(error,file){
+      if(error){
+        console.log("errrrr");
+        return;
+      }
+  response.end(file);
+
+      }
+    });
+    response.write("hhh");
+    response.end();
+  } else if (endPoint === "/node") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("hellow node");
+    response.end();
+  } else if (endPoint === "/girls") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("hellloe girls");
+    response.end();
+  }
+}

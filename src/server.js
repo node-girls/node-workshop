88d85ecf3454f0230154e332f7da11096a2e26aa
tawfiknasser <tawfiknasser@gmail.com>
2018-07-22 14:50:25 +0300

    added handler function

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 var http = require('http');
-var server=http.createServer();
+var server=http.createServer(handler);
 
 server.listen(5000,function(){
-  console.log("server is ready to go !!!!!!!! :)");
+  //console.log("server is ready to go !!!!!!!! :)");
 });
+
+function handler(request,response){
+  response.writeHead(200,{"Content-Type": "text/html"});
+  response.write("hhh");
+  response.end();
+};
